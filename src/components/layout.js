311888fe0533f7fe39e81958591c4d70101d75e6
wutong <wutong18@meituan.com>
2019-04-02 18:39:06 +0800

    add less importer. add react-bootstrap. migrating css files from template site.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,10 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "../styles/intro07.less"
+// import "../styles/main.css"
+import "../styles/main-bg.css"
+import "../styles/main-responsive.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,6 +30,12 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
         <div
           style={{
             margin: `0 auto`,
