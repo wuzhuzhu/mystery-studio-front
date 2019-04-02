18a692a13a3b059f528c45edadb48deedcbef5b0
wutong <wutong18@meituan.com>
2019-04-02 18:58:41 +0800

    use useStaticQuery to get static content.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
@@ -15,10 +15,13 @@ import "../styles/intro07.less"
 // import "../styles/main.css"
 import "../styles/main-bg.css"
 import "../styles/main-responsive.css"
+import "../styles/color.less"
+// 英文字体不适用
+// import "../styles/fonts.less"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -26,35 +29,36 @@ const Layout = ({ children }) => (
           }
         }
       }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+    `)
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossOrigin="anonymous"
+      />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        id="wrapper"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  )
+}
+
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
